6937aa8c6cfc7c365d4341455ae107ce02d4be11
yongchan <yongcgp@naver.com>
2022-07-30 00:43:55 +0900

    ps

--- silver/5525.py
+++ silver/5525.py
@@ -1,12 +1,21 @@
 n = int(input())
 m = int(input())
 s = input()
-
 answer = 0
-x = 'IO' * n + 'I'
-l = 2*n + 1
-while s:
-    if s.startswith(x):
-        answer += 1
-    s = s[1:]
+i = 0
+cnt = 0
+while i < m-1:
+    temp = s[i:i+3]
+    if temp == 'IOI':
+        i += 2
+        cnt += 1
+        
+        if cnt == n:
+            answer += 1
+            cnt -= 1
+    
+    else:
+        i += 1
+        cnt = 0
+
 print(answer)
