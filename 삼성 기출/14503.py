460f00d9882e8a3d4354ae2d99d40b5ca75f6ef3
yongchan <yongcgp@naver.com>
2022-09-07 16:55:04 +0900

    정리

--- 삼성 기출/14503.py
+++ 삼성 기출/14503.py
@@ -1,19 +1,19 @@
 n, m = map(int, input().split())
 y, x, d = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+
 cleaned = [[False] * m for _ in range(n)]
+cleaned[y][x] = True
 direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 answer = 1
-cleaned[y][x] = True
 
 def check_wall():
     global y, x
     temp_d = (d-2) % 4
-    ny = y + direction[temp_d][0]
-    nx = x + direction[temp_d][1]
-    if graph[ny][nx] == 1: 
+    y += direction[temp_d][0]
+    x += direction[temp_d][1]
+    if graph[y][x] == 1: 
         return True
-    y, x = ny, nx
     return False
 
 while True:
